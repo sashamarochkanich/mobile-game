4357f635cdd4a30157b7a7dc19c324cc8a77b441
Oleksandr Marochkanych <sashamarochkanich@gmail.com>
2017-12-10 15:56:31 +0200

    Animation

---
+++ Assets/Scripts/MainScene/DetectClics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectClics : MonoBehaviour {
+
+	public Text playTxt, gameName;
+
+	void OnMouseDown (){
+		playTxt.gameObject.SetActive (false);
+		gameName.text = "0";
+	}
+}
